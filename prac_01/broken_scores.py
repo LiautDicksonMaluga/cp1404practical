ea391feebcdd0669a14b4f0f66918f497d443515
LiautDicksonMaluga <liaut.dicksonmaluga@my.jcu.edu.au>
2021-08-21 14:42:38 +1000

    Added previous practicals to github

---
+++ prac_01/broken_scores.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+score = float(input("Enter Score: "))
+if score < 0:
+    print("Invalid score")
+else:
+    if score > 100:
+        print("Invalid score")
+    if score > 90:
+        print("Excellent")
+    if score > 50:
+        print("passable")
+    if score < 50:
+        print("bad")
