abddb021af6ec0f9a397721496df0467ef115c7f
LiautDicksonMaluga <liaut.dicksonmaluga@my.jcu.edu.au>
2021-08-21 15:02:57 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,7 +1,22 @@
 MIN_LENGTH = 4
-password = str(input("What is your password: "))
-while len(password) < 4:
-    print("Password does not meet the minimum requirements")
-    password = input("what is your password")
-for i in range(0, len(password)):
-    print("*", end="")
+
+
+def main():
+    password = get_password()
+    print_password(password)
+
+
+def get_password():
+    password = str(input("What is your password: "))
+    while len(password) < MIN_LENGTH:
+        print("Password does not meet the minimum requirements")
+        password = input("what is your password: ")
+    return password
+
+
+def print_password(password):
+    for i in range(0, len(password)):
+        print("*", end="")
+
+
+main()
